528745e77168b8f48a20a0dbd9a62c31dc534785
carlacodes <carla.griffiths@gmail.com>
2019-03-13 15:56:58 +0000

    Formatted by Louis 3.56 PM 13_03_2019

--- DinnerDirect(withOrderForm)Edited/checkout.php
+++ DinnerDirect(withOrderForm)Edited/checkout.php
@@ -165,7 +165,7 @@ if (empty($_SESSION['userID'])){
 <!-- Footer -->
 <footer class="py-5 bg-dark">
     <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+        <p class="m-0 text-center text-white">Copyright &copy; DinnersDirect 2019</p>
     </div>
     <!-- /.container -->
 </footer>
