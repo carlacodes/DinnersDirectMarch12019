b15ca41a592d1449fef1d8d9fca3051f4a724f40
carlacodes <carla.griffiths@gmail.com>
2019-03-24 15:28:56 +0000

    deleting second file

--- DinnersDirectMarch12019-master/DinnerDirect(withOrderForm)Edited/phpdata/dbCredentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DBSERVER","localhost");
-define("DBUSER","root");
-define("DBPASSWORD","root");
-define("DBNAME","dinnersdirect");
-define("DBPORT",3306); //credentials to connect to a sakila database
